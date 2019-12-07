d8ff5449aa686c538f413edd49cf735387c74a3c
Brandon Croom <38475304+jbcroom@users.noreply.github.com>
2019-12-07 11:09:56 -0500

    Updated Random Forest
    Added the 75/25 train test spllit

--- EDA/BC_Script_Updated.R
+++ EDA/BC_Script_Updated.R
@@ -53,15 +53,16 @@ reduced <- reduced %>% mutate(ID_Hispanic=recode(ID_Hispanic,
                                                `4`=1,
                                                `5`=1))
 
-skim(reduced) %>% kable()
+#######################################################################################################################
+## TREE CALCULATIONS
+#######################################################################################################################
+
+#Run with 50/50 train test split
+#######################################################################################################################
 
 # test / train data set split 50/50
 train <- reduced[1:200,]
 test <- reduced[201:452,]
-
-#######################################################################################################################
-## TREE CALCULATIONS
-#######################################################################################################################
 
 summary(train$cdx_cog)
 #Need to convert to factor for trees to work correctly
@@ -128,7 +129,88 @@ roc.perf = performance(pred, measure = "tpr", x.measure = "fpr")
 #I'm just rinsing and repeating code the produces the curve.
 auc.train <- performance(pred, measure = "auc")
 auc.train <- auc.train@y.values
-plot(roc.perf,main="AUC of Test set RF - mtry=5")
+plot(roc.perf,main="AUC of Test set RF")
+abline(a=0, b= 1)
+text(x = .40, y = .6,paste("AUC = ", round(auc.train[[1]],3), sep = ""))
+
+#Variable Importance Plots
+varImpPlot (rf.model,type=1,main="Variable Importance")
+varImpPlot (rf.model,type=2,main="Variable Importance")
+
+#Run with 75/25 train test split
+#######################################################################################################################
+
+set.seed(1234)
+trainobs=sample(seq(1,dim(reduced)[1]),round(.75*dim(reduced)[1]),replace=FALSE)
+train=reduced[trainobs,]
+test=reduced[-trainobs,]
+
+summary(train$cdx_cog)
+#Need to convert to factor for trees to work correctly
+train$cdx_cog = factor(train$cdx_cog)
+test$cdx_cog = factor(test$cdx_cog)
+summary(train$cdx_cog)
+
+#Run regular trees
+tree.medical=tree(cdx_cog~.,train)
+plot(tree.medical)
+text(tree.medical, pretty = 0)
+tree.pred=predict(tree.medical,test,type="class")
+table(tree.pred,test$cdx_cog)
+
+#Prune the trees 
+set.seed(3)
+par(mfrow=c(1,1))
+cv.medical=cv.tree(tree.medical,FUN=prune.misclass)
+names(cv.medical)
+plot(cv.medical)
+#Fit the pruned tree and visualize
+prune.medical=prune.misclass(tree.medical,best=9)
+plot(prune.medical)
+text(prune.medical,pretty=0)
+
+tree.pred=predict(prune.medical,test,type="class")
+table(tree.pred,test$cdx_cog)
+
+#We can verify the overfitting idea by saying to split the tree
+#very deep
+prune.medical=prune.misclass(tree.medical,best=9)
+plot(prune.medical)
+text(prune.medical,pretty=0)
+tree.pred=predict(prune.medical,test,type="class")
+table(tree.pred,test$cdx_cog)
+
+
+#For ROC curves on a single decision tree you need predicted probabilities to
+#use the previous R scripts.  Lets just use the example here with the last run.
+tree.pred=predict(prune.medical,test,type="vector")
+head(tree.pred)
+
+pred <- prediction(tree.pred[,2], test$cdx_cog)
+roc.perf = performance(pred, measure = "tpr", x.measure = "fpr")
+#Note in the following code the term "train" means nothing here. 
+#I'm just rinsing and repeating code the produces the curve.
+auc.train <- performance(pred, measure = "auc")
+auc.train <- auc.train@y.values
+plot(roc.perf,main="AUC of Test set of a Single Tree")
+abline(a=0, b= 1)
+text(x = .40, y = .6,paste("AUC = ", round(auc.train[[1]],3), sep = ""))
+
+
+#Execute the random forest
+rf.model = randomForest(cdx_cog~.,train,subsets=train,importance=T,ntree=100)
+fit.pred<-predict(rf.model,newdata=test,type="response")
+table(fit.pred,test$cdx_cog)
+
+#Run ROC curves for RandomForest Model
+rf.pred<-predict(rf.model,newdata=test,type="prob")
+pred <- prediction(rf.pred[,2], test$cdx_cog)
+roc.perf = performance(pred, measure = "tpr", x.measure = "fpr")
+#Note in the following code the term "train" means nothing here. 
+#I'm just rinsing and repeating code the produces the curve.
+auc.train <- performance(pred, measure = "auc")
+auc.train <- auc.train@y.values
+plot(roc.perf,main="AUC of Test set RF")
 abline(a=0, b= 1)
 text(x = .40, y = .6,paste("AUC = ", round(auc.train[[1]],3), sep = ""))
 
